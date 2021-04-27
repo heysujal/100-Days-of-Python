fee473be834a31a1e6564b874fa435e584653a37
Sujal Gupta <sujalgupta6100@gmail.com>
2021-04-27 17:08:02 +0530

    day 5

---
+++ PythonSnipps.py
@@ -0,0 +1,28 @@
+print("hello world"[0])
+# underscores are ignored
+mystery = 734_529.678
+print(mystery)
+
+score = 0
+heigth= 2.15
+isWinning = True
+
+# F string
+print(f"Your score is {score}")
+
+
+# random numbers
+# produces a number including 0 and 100
+import random
+# generates random interger
+random.randint(0,100)
+# Returns the next random floating point number between [0.0 to 1.0)
+random.random()  
+# Random
+a = ['one', 'eleven', 'twelve', 'five', 'six', 'ten']
+print(random.choice(a))
+#inbuilt python functions
+sum(a)
+len(a)
+max(a)
+min(a)
