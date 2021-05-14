c0d75cc959167991b3840f482f49c51bc1889882
Sujal Gupta <sujalgupta6100@gmail.com>
2021-05-15 03:09:15 +0530

    add pandas snipps

--- PythonSnipps.py
+++ PythonSnipps.py
@@ -1,31 +1,30 @@
 # import all at once
 from turtle import *
 
-
 print("hello world"[0])
 # underscores are ignored
 mystery = 734_529.678
 print(mystery)
 
 score = 0
-heigth= 2.15
+heigth = 2.15
 isWinning = True
 
 # F string
 print(f"Your score is {score}")
 
-
 # random numbers
 # produces a number including 0 and 100
 import random
+
 # generates random interger
-random.randint(0,100)
+random.randint(0, 100)
 # Returns the next random floating point number between [0.0 to 1.0)
-random.random()  
+random.random()
 # Random
 a = ['one', 'eleven', 'twelve', 'five', 'six', 'ten']
 print(random.choice(a))
-#Inbuilt python functions
+# Inbuilt python functions
 sum(a)
 len(a)
 max(a)
@@ -33,28 +32,28 @@ min(a)
 # append and add in lists
 fruits = []
 
-fruits+="orange"
+fruits += "orange"
 
 print(fruits)
-#['o', 'r', 'a', 'n', 'g', 'e']
+# ['o', 'r', 'a', 'n', 'g', 'e']
 
-#Importing module 
-import python_file_name
+# Importing module
 # or
-from other_python_pile import word_list,logo,stages
+from other_python_pile import word_list
+
 
 # Keyword Argument
 def greet_with_name(name, location):
-  print(f"Hello {name}")
-  print(f"Your location is {location}")
+    print(f"Hello {name}")
+    print(f"Your location is {location}")
 
-greet_with_name(location="Delhi",name="Sujal")
- 
+
+greet_with_name(location="Delhi", name="Sujal")
 
 # How to get index and the element itself without using index()
 for i in range(len(word_list):
     letter = word_list[i]
-    
+
 # Strings in python are immutable
 
 # Dictionaries
@@ -66,108 +65,118 @@ my_dict["Loop"] = "An action of doing something over and over again"
 
 # looping through a dictionary
 for key in my_dict:
-  print(key)
-  print(my_dict[key])
+    print(key)
+print(my_dict[key])
 
 #######################################
 
-#Nesting 
+# Nesting
 capitals = {
-  "France": "Paris",
-  "Germany": "Berlin",
+    "France": "Paris",
+    "Germany": "Berlin",
 }
 
-#Nesting a List in a Dictionary
+# Nesting a List in a Dictionary
 
 travel_log = {
-  "France": ["Paris", "Lille", "Dijon"],
-  "Germany": ["Berlin", "Hamburg", "Stuttgart"],
+    "France": ["Paris", "Lille", "Dijon"],
+    "Germany": ["Berlin", "Hamburg", "Stuttgart"],
 }
 
-#Nesting Dictionary in a Dictionary
+# Nesting Dictionary in a Dictionary
 
 travel_log = {
-  "France": {"cities_visited": ["Paris", "Lille", "Dijon"], "total_visits": 12},
-  "Germany": {"cities_visited": ["Berlin", "Hamburg", "Stuttgart"], "total_visits": 5},
+    "France": {"cities_visited": ["Paris", "Lille", "Dijon"], "total_visits": 12},
+    "Germany": {"cities_visited": ["Berlin", "Hamburg", "Stuttgart"], "total_visits": 5},
 }
 
-#Nesting Dictionaries in Lists
+# Nesting Dictionaries in Lists
 
 travel_log = [
-{
-  "country": "France", 
-  "cities_visited": ["Paris", "Lille", "Dijon"], 
-  "total_visits": 12,
-},
-{
-  "country": "Germany",
-  "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
-  "total_visits": 5,
-},
+    {
+        "country": "France",
+        "cities_visited": ["Paris", "Lille", "Dijon"],
+        "total_visits": 12,
+    },
+    {
+        "country": "Germany",
+        "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+        "total_visits": 5,
+    },
 ]
 
-travel_log[0] = "Sujal"  #Valid
-print(travel_log[0]) #can produce error if key not found i.e. if 0 is not any key
+travel_log[0] = "Sujal"  # Valid
+print(travel_log[0])  # can produce error if key not found i.e. if 0 is not any key
 
 # Scope
 gamelevel = 3
-enemies = ["skeleton","zombie","alien"]
+enemies = ["skeleton", "zombie", "alien"]
 
 if gamelevel < 5:
-  new_enemy  = enemies[0]
+    new_enemy = enemies[0]
 
-print(new_enemy)  #valid
+print(new_enemy)  # valid
 # Remember that in Python there is no block scope. Inside a if/else/for/while code block is the same as outside it.
-#scope with function
+# scope with function
 gamelevel = 3
-enemies = ["skeleton","zombie","alien"]
+enemies = ["skeleton", "zombie", "alien"]
 # function creates a local scope.
-def fun():
 
-  if gamelevel < 5:
-    new_enemy  = enemies[0]
-  # print(new_enemy)  #Valid
+
+def fun():
+    if gamelevel < 5:
+        new_enemy = enemies[0]
+    # print(new_enemy)  #Valid
+
+
 fun()
-print(new_enemy)  #Invalid
+print(new_enemy)  # Invalid
 
 # Using global variables can lead to bugs
-#they should be used only for making global constants
+# they should be used only for making global constants
 
 
 # This is valid code even if we dont pass enemies as argument
-enemies =1
+enemies = 1
+
 
 def inc_enemy():
     print(f"enemy is {enemies} ")
-    return enemies+100
+    return enemies + 100
+
 
 print(inc_enemy())
+
 
 # the point being we cannot modify global variable inside a function
 
 
 # Making Class
 class Sujal:
-  pass
-  # Constructor
-  def __init__(self,username,id):
-    self.username = username
-    self.id = id
-    self.followers = 0
+    pass
+
+    # Constructor
+    def __init__(self, username, id):
+        self.username = username
+        self.id = id
+        self.followers = 0
+
+
 # Without constructor
 user1 = Sujal()
 # adding attributes
 user1.id = "101"
 
-user1 = Sujal("sujal","001")
+user1 = Sujal("sujal", "001")
+
 
 # example2
 class Sujal:
-  pass
+    pass
 
-  def __init__(self, id, username):
-    self.id = id
-    self.username = username
+    def __init__(self, id, username):
+        self.id = id
+        self.username = username
 
 
 user1 = Sujal(54, "heysujal")
@@ -177,7 +186,7 @@ print(user1.username)
 
 # Creating multiple objects from same class
 all_turtles = []
-for turtle_index in range(0,6):
+for turtle_index in range(0, 6):
     new_turtle = Turtle(shape='turtle')
     # new_turtle.penup()
     # new_turtle.color(colors[turtle_index])
@@ -188,10 +197,37 @@ for turtle_index in range(0,6):
 
 # Inheritence Example
 class Food(Turtle):
-# no need to create a new_turtle from Turtle()
-  def __init__(self):
-    super().__init__()
-    self.shape('circle')
-    self.penup()
-    self.shapesize(stretch_len=0.5, stretch_wid=0.5)
+    # no need to create a new_turtle from Turtle()
+    def __init__(self):
+        super().__init__()
+        self.shape('circle')
+        self.penup()
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
 
+
+# file handling
+# In day 24
+
+# pandas library
+import pandas
+
+# whole spreadsheet = dataframe
+data = pandas.read_csv("weather_data.csv")
+print(data)  # Data table
+# column = series in pandas
+print(data["condition"])  # Column
+print(data.condition)
+# dataframe to dictionary
+data_dict = data.to_dict()
+# series to list
+data_list = data["temp"].to_list()
+# Print a row
+print(data[data.day == "Monday"])
+print(data[data["temp"] == data["temp"].max()])
+
+# Comparing for a specific column value
+data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+grey_squirrels_count = len(data[data["Primary Fur Color"] == "Gray"])
+
+# Get actual value from a column of a row
+print(desired_row.state.item())
